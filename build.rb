1381124dd1a80e9e960879771dfe4134d10d9e4a
Hanna Reitz <xanclic@xanclic.moe>
2013-03-24 13:55:21 +0100

    Initial Commit
    + Die drei wichtigsten Skripte und die Patches für GCC 4.8.0 und
      Binutils 2.23.1.

---
+++ build.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+def suicide msg
+    puts("\033[0;31;1m#{msg}\033[0m")
+    exit 1
+end
+
+def msg_status msg
+    printf('%-70s', msg)
+end
+
+def msg_done
+    puts("\033[0;32;1mdone\033[0m")
+end
+
+def msg_failed
+    puts("\033[0;31;1mfailed\033[0m")
+end
+
+def failed_hence_suicide msg
+    msg_failed
+    suicide msg
+end
+
+def link(src, dst)
+    system("#{$link} #{src.map { |f| "'#{f}'" } * ' '} -o '#{dst}' #{$linkflags}")
+end
+
+
+puts("µcelium")
+puts("=======")
+
+$hostlibgccdir = File.dirname(`gcc -m32 -print-libgcc-file-name`)
+
+$target = 'i586-pc-muxomucota'
+$prefix = Dir.pwd
+$pprefix = "#{$prefix}/usr"
+
+ENV['TARGET'] = $target
+ENV['PREFIX'] = $prefix
+
+ENV['PATH'] += ":#{$prefix}/bin:#{$prefix}/#{$target}/bin"
+
+$cc = "#{$target}-gcc"
+$link = $cc
+
+$cflags = "-std=c11 -m32 '-L#{$hostlibgccdir}' -nostdinc '-I#{$prefix}/#{$target}/include' '-I#{$prefix}/include'"
+$linkflags = "-m32 '-L#{$hostlibgccdir}' '-L#{$prefix}/#{$target}/lib' '-L#{$prefix}/lib' -lc -lgcc"
+
+ENV['CC'] = $cc
+ENV['LD'] = ENV['LINK'] = $link
+
+ENV['CFLAGS'] = $cflags
+ENV['LDFLAGS'] = ENV['LINKFLAGS'] = $linkflags
+
+
+if ARGV.empty?
+    suicide 'Usage: ./build.sh path/to/package.rb'
+end
+
+
+if File.file?(ARGV[0])
+    $pbdir = File.expand_path(File.dirname(ARGV[0]))
+    eval IO.read(ARGV[0])
+else
+    suicide "Build instructions #{ARGV[0]} not found."
+end
+
+Dir.chdir("#{$prefix}/tmp")
+pkg_download
+pkg_build
+pkg_install
---
+++ update_env.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+if ARGV.empty?
+    $stderr.puts('Usage: ./update_env.rb path/to/µxoµcota')
+    exit 1
+end
+
+if !File.directory?(ARGV[0])
+    $stderr.puts("Directory #{ARGV[0]} does not exist.")
+    exit 1
+end
+
+target = 'i586-pc-muxomucota'
+arch = 'i386'
+platform = 'pc'
+
+system("mkdir -p #{target}/lib #{target}/include")
+
+system("cp '#{ARGV[0]}/src/lib/crt/crt.o' #{target}/lib/crt0.o") or exit 1
+system("cp '#{ARGV[0]}/src/lib/libc.a'   #{target}/lib") or exit 1
+system("cp '#{ARGV[0]}/src/lib/libm.a'   #{target}/lib") or exit 1
+system("cp '#{ARGV[0]}/src/lib/libcdi.a' #{target}/lib") or exit 1
+
+system("cp -r #{['common', "arch/#{arch}", "platform/#{platform}"].map { |d| "'#{ARGV[0]}/src/include/#{d}'" } * ' '} #{target}/include") or exit 1
+
+system("echo | bin/#{target}-gcc -x c - -c -o #{target}/lib/crtbegin.o") or exit 1
+system("echo | bin/#{target}-gcc -x c - -c -o #{target}/lib/crtend.o")   or exit 1
